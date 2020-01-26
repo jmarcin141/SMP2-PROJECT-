493e70748de9c1c2a83c23484f7a0aa846a937cd
Grzegorz Jurek <grzegorz.jurek159@gmail.com>
2020-01-26 16:23:32 +0100

    Configuration RC5 receiver

--- irReceiver.h
+++ irReceiver.h
@@ -11,6 +11,17 @@ void resetTimer();
 
 void TPMInitialize();
 
+void ToggleInterruptEdge();
+
+uint8_t irGetCommand();
+uint8_t irGetAddress();
+uint8_t irGetStatus();
+
+void PORTA_IRQHandler();
+
+//void PitInitialize(void);
+//void PIT_IRQHandler();
+
 
 
 #endif
