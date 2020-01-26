3889014eab8dab8e18ca137ea6c448c65933727c
Grzegorz Jurek <grzegorz.jurek159@gmail.com>
2020-01-26 16:17:16 +0100

    Added engine steering library, fixed some bugs

--- engine.c
+++ engine.c
@@ -1,17 +1,19 @@
 #include "engine.h"
 #include "PWM.h"
 
-
+char ENGINE0_STATUS = 0; // ENABLE/DISABLE ENGINE 
+char ENGINE1_STATUS = 0;
+//char ENGINEX_STATUS = 0;
 
 void engineInitialize(){
 	PWMInitialize();
 }
 
-uint16_t engineStatus(uint8_t id_engine){
+char engineStatus(uint8_t id_engine){
 	
 			switch (id_engine){
 				case ENGINE0:
-					
+					return ENGINE0_STATUS;
 				break;
 				
 				case ENGINE1:
@@ -30,14 +32,18 @@ void engineLeft(uint8_t id_engine, uint16_t channel_source){
 	
 			switch (id_engine){
 				case ENGINE0:
-					PWMsetCh1(channel_source);
-					PWMsetCh2(0);
+					if (ENGINE0_STATUS == 1){
+						PWMsetCh1(channel_source);
+						PWMsetCh2(0);
+					}
 				break;
 				
 				case ENGINE1:
+					if (ENGINE1_STATUS == 1){
 					/*
 						PWMsetCh3.. channel 4. in the future...
 					*/
+					}
 				break;
 				
 				default:
@@ -50,11 +56,36 @@ void engineRight(uint8_t id_engine, uint16_t channel_source){
 
 				switch (id_engine){
 				case ENGINE0:
-					PWMsetCh2(channel_source);
-					PWMsetCh1(0);
+					if (ENGINE0_STATUS == 1){
+						PWMsetCh2(channel_source);
+						PWMsetCh1(0);
+					}
 				break;
 				
 				case ENGINE1:
+					if (ENGINE0_STATUS == 1){
+					/*
+						PWMsetCh3.. channel 4. in the future...
+					*/
+					}
+				break;
+				
+				default:
+					// zabezpieczenie, np wylacz wszystkie silniki?
+				break;
+			}
+}
+
+
+void engineStart(uint8_t id_engine){
+
+				switch (id_engine){
+				case ENGINE0:
+					ENGINE0_STATUS = 1;
+				break;
+				
+				case ENGINE1:
+					ENGINE1_STATUS = 1;
 					/*
 						PWMsetCh3.. channel 4. in the future...
 					*/
@@ -72,9 +103,11 @@ void engineStop(uint8_t id_engine){
 				case ENGINE0:
 					PWMsetCh2(0);
 					PWMsetCh1(0);
+					ENGINE0_STATUS = 0;
 				break;
 				
 				case ENGINE1:
+					ENGINE1_STATUS = 0;
 					/*
 						PWMsetCh3.. channel 4. in the future...
 					*/
